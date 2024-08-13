bbfdd18e016453d704232597182007f64f825ed2
Devpe007 <anddev007pe@gmail.com>
2024-08-13 09:20:50 -0300

    creation route and listing

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import http from 'node:http'
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/users') {
+    return response.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return response.end('Criação de usuário')
+  }
+
   return response.end('Hello World')
 })
 
-server.listen(3333)
+server.listen(3333, () => console.log('Server is running on port 3333'))
 
