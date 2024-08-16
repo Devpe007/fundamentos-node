c75737ad36947f21e16acc7db9a525df46266908
Devpe007 <anddev007pe@gmail.com>
2024-08-16 16:05:49 -0300

    database implementation

--- src/server.js
+++ src/server.js
@@ -1,36 +1,33 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 
-const users = [];
+import { json } from './middlewares/json.js'
+import { Database } from './database.js';
+
+const database = new Database()
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
-  const buffers = []
-
-  for await (const chunk of request) {
-    buffers.push(chunk)
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    request.body = null
-  }
+  await json(request, response)
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
+
     return response
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = request.body;
 
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return response.writeHead(201).end()
   }
